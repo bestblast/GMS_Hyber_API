c60c45791793e1e6a9350554325b4320f44035c6
bestblast <blast@ukr.net>
2022-02-10 18:19:40 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,13 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('_ext'))
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions # coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['jsonlexer']
